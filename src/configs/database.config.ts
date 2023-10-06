5467a348475ee585e94b58a60bb12f28220fb061
Darosss <daroing12@gmail.com>
2023-10-06 11:21:39 +0200

    refactor: replace process.env variables with variables from env-variables

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -2,16 +2,24 @@ import 'dotenv/config';
 import * as path from 'path';
 import { DataSource } from 'typeorm';
 import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import {
+  NODE_ENV,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_USERNAME,
+  POSTGRES_PASSWORD,
+  POSTGRES_DATABASE,
+} from './env-variables';
 
-const isDevelopment = process.env.NODE_ENV === 'development';
+const isDevelopment = NODE_ENV === 'development';
 
 export const databaseConfig: PostgresConnectionOptions = {
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: parseInt(process.env.POSTGRES_PORT, 10),
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  host: POSTGRES_HOST,
+  port: parseInt(POSTGRES_PORT, 10),
+  username: POSTGRES_USERNAME,
+  password: POSTGRES_PASSWORD,
+  database: POSTGRES_DATABASE,
   entities: [`${path.dirname(__dirname)}/**/*.entity.{ts,js}`],
   logging: isDevelopment,
   synchronize: false,
